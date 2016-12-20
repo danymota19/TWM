e2124ab075256ea5e945ff3b9c0f060f22f25f5f
danymota19 <danymota19@gmail.com>
2016-12-20 17:53:51 -0600

    reelocation

---
+++ SW/TWM/Login/Logic/Login.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TWM.Logic
+{
+    public class Login
+    {
+        public void validatePassword(String password) {
+            Console.Write("dany");
+        }
+    }
+}
